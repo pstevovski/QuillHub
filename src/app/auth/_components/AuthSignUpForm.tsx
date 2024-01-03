f0372477a319a05d78eaf17e4ce6c7f4b8b6948b
Petar Stevovski <petar@codeart.mk>
2024-01-03 16:53:37 +0100

    Added client component for handling the Auth - Sign Up form and added its usage on the Auth Sign Up page

---
+++ src/app/auth/_components/AuthSignUpForm.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+// Components
+import Button from "@/components/Buttons/Button";
+import FormPasswordInput from "@/components/Form/FormPasswordInput";
+import FormTextInput from "@/components/Form/FormTextInput";
+import Link from "next/link";
+
+// Validation schemas
+import { AuthSignUpFields, AuthSignUpSchema } from "@/zod/auth";
+
+// Forms
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+export default function AuthSignUpForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AuthSignUpFields>({
+    defaultValues: {
+      email: "",
+      first_name: "",
+      last_name: "",
+      password: "",
+      confirm_password: "",
+    },
+    resolver: zodResolver(AuthSignUpSchema),
+  });
+
+  // todo: connect with API and send actual request
+  const handleSignUp: SubmitHandler<AuthSignUpFields> = (credentials) => {
+    console.log("CREDENTIALS: ", credentials);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleSignUp)} autoComplete="">
+      <FormTextInput
+        register={register("email")}
+        error={errors.email}
+        placeholder="your@email.com"
+        autoComplete="new-password"
+      />
+      <FormTextInput
+        register={register("first_name")}
+        error={errors.first_name}
+        placeholder="John"
+        autoComplete="new-password"
+      />
+      <FormTextInput
+        register={register("last_name")}
+        error={errors.last_name}
+        placeholder="Doe"
+        autoComplete="new-password"
+      />
+      <FormPasswordInput
+        register={register("password")}
+        error={errors.password}
+        modifierClass="mb-8"
+      />
+      <FormPasswordInput
+        register={register("confirm_password")}
+        error={errors.confirm_password}
+        modifierClass="mb-8"
+      />
+
+      <Button type="submit" variant="primary" size="full" modifierClass="mb-4">
+        Sign Up
+      </Button>
+
+      <div className="flex justify-center">
+        <Link
+          className=" text-slate-400 text-sm hover:text-slate-600 duration-200"
+          href="/auth/signin"
+        >
+          Go Back
+        </Link>
+      </div>
+    </form>
+  );
+}
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,8 +1,12 @@
+import AuthSignUpForm from "../_components/AuthSignUpForm";
+
 export default function AuthSignUpPage() {
   return (
-    <div className="text-center">
-      <h1 className="text-3xl font-semibold mb-2">Sign Up</h1>
-      <p>Create your new account </p>
+    <div className="w-full">
+      <h1 className="text-center text-3xl font-semibold mb-2">Sign Up</h1>
+      <p className="text-center mb-5">Create your new account </p>
+
+      <AuthSignUpForm />
     </div>
   );
 }
