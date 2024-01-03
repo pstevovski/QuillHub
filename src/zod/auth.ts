5d1bd70ddbf75bdf6f56f6a830bfde543ec716b1
Petar Stevovski <petar@codeart.mk>
2024-01-03 16:52:40 +0100

    Added validation schema and type definition for Auth - Sign Up from fields

--- src/zod/auth.ts
+++ src/zod/auth.ts
@@ -13,3 +13,34 @@ export const AuthSignInSchema = z.object({
 });
 
 export type AuthSignInFields = z.infer<typeof AuthSignInSchema>;
+
+/*====================================
+  AUTHENTICATION: SIGN UP FORM
+=====================================*/
+export const AuthSignUpSchema = z
+  .object({
+    email: z
+      .string({ required_error: "Please enter your email address" })
+      .email({ message: "Please enter a valid email address" }),
+    first_name: z
+      .string({ required_error: "Please enter your first name" })
+      .min(1, "Please enter your first name"),
+    last_name: z
+      .string({ required_error: "Please enter your last name" })
+      .min(1, "Please enter your last name"),
+    password: z
+      .string({ required_error: "Please enter your password" })
+      .min(6, "Password must be at least 6 characters long!"),
+    confirm_password: z
+      .string({ required_error: "Please confirm your password" })
+      .min(6, "Password must be at least 6 characters long!"),
+  })
+  .refine(
+    (userDetails) => userDetails.password === userDetails.confirm_password,
+    {
+      message: "Passwords do not match!",
+      path: ["confirm_password"],
+    }
+  );
+
+export type AuthSignUpFields = z.infer<typeof AuthSignUpSchema>;
