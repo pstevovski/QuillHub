8eeb2ff7e1ba7989abed8dad2e23267f1e468802
Petar Stevovski <petar@codeart.mk>
2024-01-03 16:25:54 +0100

    Added AuthSignInForm client component with 'react-hook-form' connected and added it to AuthSignIn page

---
+++ src/app/auth/_components/AuthSignInForm.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+// Components
+import Button from "@/components/Buttons/Button";
+import FormPasswordInput from "@/components/Form/FormPasswordInput";
+import FormTextInput from "@/components/Form/FormTextInput";
+
+// Validation schemas
+import { AuthSignInFields, AuthSignInSchema } from "@/zod/auth";
+
+// Forms
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+export default function AuthSignInForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AuthSignInFields>({
+    defaultValues: { email: "", password: "" },
+    resolver: zodResolver(AuthSignInSchema),
+  });
+
+  // todo: connect with API and send actual request
+  const handleSignIn: SubmitHandler<AuthSignInFields> = (credentials) => {
+    console.log("CREDENTIALS: ", credentials);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleSignIn)}>
+      <FormTextInput
+        register={register("email")}
+        error={errors.email}
+        placeholder="your@email.com"
+      />
+      <FormPasswordInput
+        register={register("password")}
+        error={errors.password}
+      />
+
+      <Button type="submit" variant="primary" size="full">
+        Sign In
+      </Button>
+    </form>
+  );
+}
--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,8 +1,14 @@
+import AuthSignInForm from "../_components/AuthSignInForm";
+
 export default function AuthSignInPage() {
   return (
-    <div className="text-center">
-      <h1 className="text-3xl font-semibold mb-2">Sign In</h1>
-      <p>Sign in to access all features of the application</p>
+    <div>
+      <h1 className="text-center text-3xl font-semibold mb-2">Sign In</h1>
+      <p className="text-center mb-5">
+        Sign in to access all features of the application
+      </p>
+
+      <AuthSignInForm />
     </div>
   );
 }
