1488fe82e2dddacb6f1d5bd9cc56ae0dae3ea3ac
Petar Stevovski <petar@codeart.mk>
2024-01-04 14:17:09 +0100

    Added drizzle configuration file

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./src/db/schema/*",
+  out: "./src/db/migrations",
+  driver: "mysql2",
+  dbCredentials: {
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME!,
+    password: process.env.DB_PASSWORD,
+  },
+} satisfies Config;
