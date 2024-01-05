0faf3bbbda01594415efb69b6ef9cad81cd36c7f
Petar Stevovski <petar@codeart.mk>
2024-01-05 12:13:55 +0100

    Corrected 'signUp' method from the Auth service

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -1,7 +1,6 @@
 import db from "@/db/connection";
 import { UserNew, users } from "@/db/schema/users";
 import { hashUserPassword } from "@/utils/bcrypt";
-import { AuthSignUpSchema } from "@/zod/auth";
 
 /**
  * Service used for handling Authentication functionality
@@ -9,31 +8,15 @@ import { AuthSignUpSchema } from "@/zod/auth";
 class Auth {
   async signUp(newUser: UserNew) {
     try {
-      // todo: rethink approach
-      // todo: fix parsing of the received fields
-      const validateProvidedDetails = AuthSignUpSchema.safeParse({
-        email: newUser.email,
-        first_name: newUser.first_name,
-        last_name: newUser.last_name,
-        password: newUser.password,
-      });
-
-      if (!validateProvidedDetails.success) {
-        return {
-          errors: validateProvidedDetails.error.flatten().fieldErrors,
-          message:
-            "Missing or incorrect fields provided! Failed creating new user.",
-        };
-      }
-
       // Hash the password before storing the data in the database
       const hashedPassword: string = await hashUserPassword(newUser.password);
-      console.log("hashed password", hashedPassword);
 
       // Save the user in the database
       await db.insert(users).values({ ...newUser, password: hashedPassword });
     } catch (error: any) {
-      console.log("Failed creating new user!", error?.message);
+      // This would be then saved in a log (e.g. Sentry)
+      console.error(`Failed creating new user: ${error.message}`);
+      throw new Error("Failed creating new user!");
     }
   }
 }
