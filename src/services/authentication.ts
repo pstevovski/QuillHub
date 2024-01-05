9f05232de07e8e4c96e43c09e3efb5b62178bec6
Petar Stevovski <petar@codeart.mk>
2024-01-05 16:42:22 +0100

    Added 'forgotPassword' auth service that will send an email containing a password reset link and store the details for the password reset request in the database

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -1,7 +1,10 @@
+import "dotenv/config";
+
 import db from "@/db/connection";
-import { UserNew, users } from "@/db/schema/users";
+import { UserNew, userPasswordResets, users } from "@/db/schema/users";
 import { hashUserPassword } from "@/utils/bcrypt";
 import handleErrorMessage from "@/utils/handleErrorMessage";
+import nodemailer from "nodemailer";
 
 /**
  * Service used for handling Authentication functionality
@@ -17,15 +20,60 @@ class Auth {
     } catch (error) {
       const errorMessage: string = handleErrorMessage(error);
 
-      // This would be then saved in a log (e.g. Sentry)
+      // todo: Should be saved using some logging service
       console.error(`Failed creating new user: ${errorMessage}`);
 
-      // Error to be thrown to the route handler and shown in the UI
       if (errorMessage.toLowerCase().startsWith("duplicate entry")) {
         throw new Error("User already exists!");
       } else {
         throw new Error("Failed creating new user!");
       }
+    }
+  }
+
+  async forgotPassword(email: string) {
+    try {
+      // Generate a unique token that will be associated
+      // with each unique email for which a password reset was requested
+      const token: string = crypto.randomUUID();
+
+      // Store the request that was made for forgotten password
+      await db.insert(userPasswordResets).values({ email, token });
+
+      // Send the email
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.GMAIL_USERNAME,
+          pass: process.env.GMAIL_APP_PASSWORD,
+        },
+      });
+
+      await transporter.sendMail({
+        from: '"Quillhub" info@quillhub.com',
+        to: email,
+        subject: "Password Reset",
+        html: `
+          <div>
+            <h1>Quillhub</h1>
+            <p><em>Unfold Your Imagination, Share Your Universe</em></p>
+            <h3>You have requested password reset for your accout</h3>
+            <br/>
+            <a href="${process.env.NEXT_PUBLIC_EMAIL_REDIRECT_URL}/auth/reset-password?token=${token}">Click here to reset your password</a>
+            <br/>
+            <br/>
+            <br/>
+            <hr/>
+            <p>If you did not request password reset for your account, please ignore this email.</p>
+          </div>
+        `,
+      });
+    } catch (error) {
+      const errorMessage: string = handleErrorMessage(error);
+      console.log(`Failed sending password reset email: ${errorMessage}`);
+      throw new Error("Failed sending password reset email!");
     }
   }
 }
