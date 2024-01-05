df4eced864a8732192b8d77c43a427492dc10ba7
Petar Stevovski <petar@codeart.mk>
2024-01-05 14:01:38 +0100

    Added utility function that will be used to handle the fetch requests

---
+++ src/utils/fetchHandler.ts
@@ -0,0 +1,37 @@
+import handleErrorMessage from "./handleErrorMessage";
+
+type Methods = "GET" | "POST" | "PUT" | "DELETE";
+
+export default async function fetchHandler(
+  method: Methods,
+  endpoint: string,
+  body: Record<string, unknown> | undefined
+) {
+  try {
+    // todo: handling of access token
+    const URL: string = `${process.env.NEXT_PUBLIC_API_BASE_URL}/${endpoint}`;
+
+    const response = await fetch(URL, {
+      method,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      ...(body && { body: JSON.stringify(body) }),
+    });
+
+    // Exit function if response is 204 - No Content
+    if (response.status === 204) return;
+
+    // Parse the received data object
+    const data = await response.json();
+
+    // Throw error if the request has failed
+    if (!response.ok) throw new Error(data.error);
+
+    // Return the data if everything was ok
+    return data;
+  } catch (error) {
+    throw new Error(handleErrorMessage(error));
+  }
+}
