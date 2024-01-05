b888130319f2e80610d1fbd1341f0c6c24ddfcda
Petar Stevovski <petar@codeart.mk>
2024-01-05 12:15:01 +0100

    Updated 'auth/signup' API route so it now handles payload validation and throws an error preventing saving the entry to the database if anything is wrong. Updated handling of error messages and status codes

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,18 +1,35 @@
 import AuthService from "@/services/authentication";
+import { AuthSignUpSchema } from "@/zod/auth";
 
 export async function POST(request: Request) {
   try {
     const user = await request.json();
+
+    // Check the received payload and if its not valid,
+    // throw an error and prevent saving the entry to the database
+    const userPayload = AuthSignUpSchema.safeParse({
+      email: user.email,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      password: user.password,
+      confirm_password: user.confirm_password,
+    });
+
+    if (!userPayload.success) {
+      return Response.json(
+        { error: "Invalid values provided!" },
+        { status: 422 }
+      );
+    }
+
+    // Save the user in the database
     await AuthService.signUp(user);
+
     return Response.json(
       { message: "User registered successfully!" },
       { status: 200 }
     );
-  } catch (error) {
-    // todo: handle the different possible error scenarios
-    return Response.json(
-      { error: "Failed creating new user!" },
-      { status: 400 }
-    );
+  } catch (error: any) {
+    return Response.json({ error: error.message }, { status: 500 });
   }
 }
