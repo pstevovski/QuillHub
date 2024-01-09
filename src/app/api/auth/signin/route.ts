c6dc3a36b2afd4b850dcd12d643236a20b30d8d1
Petar Stevovski <petar@codeart.mk>
2024-01-09 16:32:55 +0100

    Added 'Auth - Signin' route (API endpoint) which

---
+++ src/app/api/auth/signin/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { signToken } from "@/utils/jwt";
+import handleErrorMessage from "@/utils/handleErrorMessage";
+
+export async function POST(request: NextRequest) {
+  const payload = await request.json();
+
+  // todo: validate the payload that is received
+  // todo: add check for the email/password combination
+  // todo: add 'signin' method in the auth service to handle this
+  // todo: move signing the token within that method
+  // todo: update the payload that will be saved within the token
+
+  // Generate a new token that will be saved as HttpOnly cookie
+  try {
+    await signToken(payload);
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    console.error(`Sign In failed: ${handleErrorMessage(error)}`);
+    return Response.json(
+      { error: "Sign In failed. Something went wrong!" },
+      { status: 500 }
+    );
+  }
+}
