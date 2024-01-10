097718c46b352af6a4a1f60b1b805dcc0be6df28
Petar Stevovski <petar@codeart.mk>
2024-01-10 10:21:17 +0100

    Updated middleware to make use of the newly added TokenService; updated handling of redirects when user has a valid token and tries accessing some of the authentication pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,37 @@
-import { NextRequest, NextResponse } from "next/server";
-import { JWT_USER_TOKEN_NAME, verifyToken } from "./utils/jwt";
+// NextJS utilities
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import { cookies } from "next/headers";
 
+// Services
+import TokenService from "./services/token";
+
 export async function middleware(request: NextRequest) {
-  const authToken = cookies().get(JWT_USER_TOKEN_NAME)?.value || "";
+  const authToken = cookies().get(TokenService.TOKEN_NAME)?.value || "";
 
   // Do not take the authenication token into consideration if
   // the user is currently on one of the the authentication pages
   if (request.nextUrl.pathname.startsWith("/auth") && !authToken) return;
 
-  // Verify the token that the user has saved as HttpOnly cookie
-  const verifiedToken = await verifyToken(authToken).catch((error) => {
-    console.error("Failed verifying access token: ", error.message);
-  });
+  // Verify that the token that exists as HttpOnly cookie
+  const verifiedToken = await TokenService.verifyToken(authToken).catch(
+    (error) => {
+      console.error("Failed verifying access token: ", error.message);
+    }
+  );
 
   // If the token has expired then redirect the user back to the signin page
   if (!verifiedToken) {
     return NextResponse.redirect(new URL("/auth/signin", request.url));
   }
 
-  // If everything is fine, allow access to the protected routes
+  // If user tried accessing some of the authentication pages
+  // while having a valid token, redirect to default (home) page
+  if (request.nextUrl.pathname.startsWith("/auth") && verifiedToken) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // If everything is fine, allow access to the route that the user has requested
   return NextResponse.next();
 }
 
