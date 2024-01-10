0c50dfc200b25f610f84883d38b95aa9e9011c91
Petar Stevovski <petar@codeart.mk>
2024-01-10 14:01:44 +0100

    Updates to the FormCheckbox component

--- src/components/Form/FormCheckbox.tsx
+++ src/components/Form/FormCheckbox.tsx
@@ -5,25 +5,28 @@ import { UseFormRegisterReturn } from "react-hook-form";
 
 interface FormCheckboxInputProps extends InputHTMLAttributes<HTMLInputElement> {
   register: UseFormRegisterReturn;
-  label: string;
-  containerModifierClass?: string;
+  text: string;
+  modifierClass?: string;
 }
 
 export default function FormCheckbox({
-  label,
+  text,
   register,
-  containerModifierClass = "",
+  modifierClass = "",
   ...props
 }: FormCheckboxInputProps) {
   return (
-    <div className={cn("inline-flex items-center", containerModifierClass)}>
+    <div
+      className={cn("inline-flex items-center", modifierClass)}
+      data-tooltip-target="tooltip"
+    >
       <label
         className="relative flex items-centerrounded-full cursor-pointer"
         htmlFor="check"
       >
         <input
           type="checkbox"
-          className="peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border transition-all hover:bg-slate-50 checked:border-blue-400 checked:bg-blue-400 checked:hover:bg-blue-500 checked:hover:border-blue-500 disabled:bg-slate-100 disabled:border-slate-200 disabled:cursor-not-allowed outline-blue-500"
+          className="peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border transition-all hover:bg-slate-50 checked:border-blue-400 checked:bg-blue-400 checked:hover:bg-blue-500 checked:hover:border-blue-500 disabled:bg-slate-100 disabled:border-slate-200 disabled:cursor-not-allowed outline-blue-500 duration-200"
           id="check"
           {...register}
           {...props}
@@ -37,25 +40,24 @@ export default function FormCheckbox({
             viewBox="0 0 20 20"
             fill="currentColor"
             stroke="currentColor"
-            stroke-width="1"
+            strokeWidth="1"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </span>
-      </label>
 
-      <label
-        className={cn(
-          "mt-px ml-2 font-light text-sm text-gray-700 cursor-pointer select-none",
-          props.disabled ? "text-slate-400" : ""
-        )}
-        htmlFor="check"
-      >
-        {label}
+        <p
+          className={cn(
+            "ml-2 font-light text-sm text-slate-400 hover:text-slate-500 cursor-pointer select-none duration-200",
+            props.disabled ? "text-slate-400" : ""
+          )}
+        >
+          {text}
+        </p>
       </label>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,19 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55) forwards",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
