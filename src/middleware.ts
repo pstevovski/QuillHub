6134600d6e86ada45cb2ee123464a8ece752eaf7
Petar Stevovski <petar@codeart.mk>
2024-01-11 15:11:58 +0100

    Updated middleware: - Added functionality to handle checks if the route that the user tried accessing is part of the protected routes (page or API endpoint) - Updated handling of checks if the user has a valid authentication token - Updated middleware's 'matcher' configuration object so it will be triggered on all routes except for routes that serve items from '_next' or 'public' folders

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,13 @@ import { cookies } from "next/headers";
 
 // Services
 import TokenService from "./services/token";
+import { handleCheckIfProtectedRoute } from "./utils/protectedRoutes";
+
+// Do not invoke the middleware function on NextJS and items served from "public" (e.g. favicon)
+// On all other defined routes the middleware should be triggered
+export const config = {
+  matcher: "/((?!static|.*\\..*|_next).*)",
+};
 
 export async function middleware(request: NextRequest) {
   const authToken = cookies().get(TokenService.TOKEN_NAME)?.value || "";
@@ -13,29 +20,32 @@ export async function middleware(request: NextRequest) {
   // the user is currently on one of the the authentication pages
   if (request.nextUrl.pathname.startsWith("/auth") && !authToken) return;
 
-  // Verify that the token that exists as HttpOnly cookie
-  const verifiedToken = await TokenService.verifyToken(authToken).catch(
-    (error) => {
-      console.error("Failed verifying access token: ", error.message);
-    }
+  // Verify the token that the user sent in the request
+  // prettier-ignore
+  const hasValidToken = await TokenService
+    .verifyToken(authToken)
+    .catch((error) => console.error("Failed verifying access token: ", error.message)
   );
 
-  // If the token has expired then redirect the user back to the signin page
-  if (!verifiedToken) {
-    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  // If user does not have a valid token and tried accessing proected route
+  // then either redirect to signin page or show JSON message if requested route was API endpoint
+  const isProtectedRoute: boolean = handleCheckIfProtectedRoute(request);
+  if (!hasValidToken && isProtectedRoute) {
+    if (request.nextUrl.pathname.startsWith("/api")) {
+      return NextResponse.json(
+        { message: "You are not authenticated", status: 401 },
+        { status: 401 }
+      );
+    } else {
+      return NextResponse.redirect(new URL("/auth/signin", request.url));
+    }
   }
 
-  // If user tried accessing some of the authentication pages
-  // while having a valid token, redirect to default (home) page
-  if (request.nextUrl.pathname.startsWith("/auth") && verifiedToken) {
+  // Redirect the user to the home page when trying to access Authentication pages with valid token
+  if (request.nextUrl.pathname.startsWith("/auth") && hasValidToken) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
   // If everything is fine, allow access to the route that the user has requested
   return NextResponse.next();
 }
-
-// todo: set configuration for which routes should the middleware be enabled
-export const config = {
-  matcher: ["/auth/:path*", "/protected"],
-};
