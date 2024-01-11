85152b9336b746f67642ab9854d1fd32eddbf37e
Petar Stevovski <petar@codeart.mk>
2024-01-11 14:06:03 +0100

    Updated response that is returned from the 'auth/signout' endpoint; redirect the user to the 'auth/signin' page using client side navigation upon successful signout action

--- src/app/api/auth/signout/route.tsx
+++ src/app/api/auth/signout/route.tsx
@@ -1,17 +1,15 @@
 import AuthService from "@/services/authentication";
 import handleErrorMessage from "@/utils/handleErrorMessage";
-import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 
 export async function POST() {
   try {
     await AuthService.signOut();
+    return NextResponse.json({ message: "User signed out" }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { error: handleErrorMessage(error) },
       { status: 400 }
     );
   }
-
-  redirect("/auth/signin");
 }
--- src/app/auth/_components/AuthSignOut.tsx
+++ src/app/auth/_components/AuthSignOut.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import Button from "@/components/Buttons/Button";
+// Utilities & Hooks
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 import fetchHandler from "@/utils/fetchHandler";
 import handleErrorMessage from "@/utils/handleErrorMessage";
-import { toast } from "sonner";
+
+// Components
+import Button from "@/components/Buttons/Button";
 
 export default function AuthSignOut() {
+  const router = useRouter();
+
   const handleSignOut = async () => {
     try {
       await fetchHandler("POST", "auth/signout", undefined);
+      router.push("/auth/signin");
     } catch (error) {
       toast.error(handleErrorMessage(error));
     }
