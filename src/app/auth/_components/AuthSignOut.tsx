416cf31d0be9b854f99b919cb216bfd31daabf18
Petar Stevovski <petar@codeart.mk>
2024-01-12 16:57:21 +0100

    Updated AuthSignOut component to be able to receive 'authenticatedUser' prop which contains the details from the decoded jwt token; updated TODO file

--- src/app/auth/_components/AuthSignOut.tsx
+++ src/app/auth/_components/AuthSignOut.tsx
@@ -1,29 +1,37 @@
 "use client";
 
 // Utilities & Hooks
-import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import fetchHandler from "@/utils/fetchHandler";
 import handleErrorMessage from "@/utils/handleErrorMessage";
 
 // Components
 import Button from "@/components/Buttons/Button";
+import { JWTPayload } from "jose";
 
-export default function AuthSignOut() {
-  const router = useRouter();
-
+export default function AuthSignOut({
+  authenticatedUser,
+}: {
+  authenticatedUser: JWTPayload | undefined;
+}) {
   const handleSignOut = async () => {
     try {
       await fetchHandler("POST", "auth/signout", undefined);
-      router.push("/auth/signin");
+
+      // note: this is a "hacky" solution to
+      // be able to clear out the received details for the authenticated user
+      window.location.replace("/auth/signin");
     } catch (error) {
       toast.error(handleErrorMessage(error));
     }
   };
 
   return (
-    <Button type="button" handleOnClick={handleSignOut}>
-      Sign Out
-    </Button>
+    <div>
+      <p>User is {authenticatedUser?.role_id === 2 ? "Admin" : "Regular"}</p>
+      <Button type="button" handleOnClick={handleSignOut}>
+        Sign Out
+      </Button>
+    </div>
   );
 }
