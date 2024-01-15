92c09c140126b84c344f2a249011cfb251a549eb
Petar Stevovski <petar@codeart.mk>
2024-01-15 16:28:18 +0100

    Reverted middleware changes that were used for checking if user has access token when not accessing 'auth/signin' page and updated variable names

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 // NextJS utilities
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 
 // Services
 import TokenService from "./services/token";
@@ -13,13 +14,21 @@ export const config = {
 };
 
 export async function middleware(request: NextRequest) {
+  const accessToken = cookies().get(TokenService.ACCESS_TOKEN_NAME)?.value;
+
+  // Do not take the authenication token into consideration if
+  // the user is currently on one of the the authentication pages
+  if (request.nextUrl.pathname.startsWith("/auth") && !accessToken) return;
+
   // Verify the token that the user sent in the request
-  const hasValidToken = await TokenService.verifyToken();
+  const hasValidAccessToken = await TokenService.verifyToken(accessToken).catch(
+    (error) => console.error("Failed verifying access token: ", error.message)
+  );
 
   // If user does not have a valid token and tried accessing proected route
   // then either redirect to signin page or show JSON message if requested route was API endpoint
   const isProtectedRoute: boolean = handleCheckIfProtectedRoute(request);
-  if (!hasValidToken && isProtectedRoute) {
+  if (!hasValidAccessToken && isProtectedRoute) {
     if (request.nextUrl.pathname.startsWith("/api")) {
       return NextResponse.json(
         { message: "You are not authenticated", status: 401 },
@@ -31,7 +40,7 @@ export async function middleware(request: NextRequest) {
   }
 
   // Redirect the user to the home page when trying to access Authentication pages with valid token
-  if (request.nextUrl.pathname.startsWith("/auth") && hasValidToken) {
+  if (request.nextUrl.pathname.startsWith("/auth") && hasValidAccessToken) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
