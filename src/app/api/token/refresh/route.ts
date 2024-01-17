f5f7531cd438b653baad8b2217329ca7db1f3f85
Petar Stevovski <stevovski.petar@gmail.com>
2024-01-17 23:40:49 +0100

    Updated 'token/refresh' endpoint to make it suitable when called either from middleware or from client, and it passes down the correct 'refreshToken' value

--- src/app/api/token/refresh/route.ts
+++ src/app/api/token/refresh/route.ts
@@ -1,11 +1,23 @@
 import TokenService from "@/services/token";
 import handleErrorMessage from "@/utils/handleErrorMessage";
-import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(request: NextRequest) {
   try {
-    const refreshedToken = await TokenService.refreshAccessToken();
-    return NextResponse.json({ expiresTime: refreshedToken }, { status: 200 });
+    // If the payload contains a "refreshToken" value, then the request  originates from the "middleware"
+    // If it does not contain that value, then the request originates from the client and it
+    // should read the cookies that were sent from the browser with the request
+    const payload = await request.json();
+    const cookieRefreshToken = cookies().get(
+      TokenService.REFRESH_TOKEN_NAME
+    )?.value;
+
+    const { token, expires } = await TokenService.refreshAccessToken(
+      payload?.refreshToken || cookieRefreshToken
+    );
+
+    return NextResponse.json({ token, expires }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { error: handleErrorMessage(error) },
