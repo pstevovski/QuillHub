76fdb6efaa63e9a0db6855fec92e267162bc148a
Petar Stevovski <stevovski.petar@gmail.com>
2024-01-19 01:32:30 +0100

    Updated 'token/refresh' endpoint to only use the refresh token from the payload from the middleware

--- src/app/api/token/refresh/route.ts
+++ src/app/api/token/refresh/route.ts
@@ -1,20 +1,12 @@
 import TokenService from "@/services/token";
 import handleErrorMessage from "@/utils/handleErrorMessage";
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
-    // If the payload contains a "refreshToken" value, then the request  originates from the "middleware"
-    // If it does not contain that value, then the request originates from the client and it
-    // should read the cookies that were sent from the browser with the request
     const payload = await request.json();
-    const cookieRefreshToken = cookies().get(
-      TokenService.REFRESH_TOKEN_NAME
-    )?.value;
-
     const { token, expires } = await TokenService.refreshAccessToken(
-      payload?.refreshToken || cookieRefreshToken
+      payload?.refreshToken
     );
 
     return NextResponse.json({ token, expires }, { status: 200 });
