f91721edce2308d1d6f048053a83d9336c5afb74
Petar Stevovski <stevovski.petar@gmail.com>
2024-01-20 00:55:58 +0100

    WIP: Main application layout and homepage

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import TokenService from "@/services/token";
 import Link from "next/link";
 import AuthSignOut from "../auth/_components/AuthSignOut";
+import { RiQuillPenLine as QuillHubLogo } from "react-icons/ri";
 
 export default async function MainLayout({
   children,
@@ -9,23 +10,26 @@ export default async function MainLayout({
 }) {
   // Get the currently logged in user from the database
   const token = await TokenService.decodeToken();
-  console.log("token", token);
 
   return (
     <>
-      <header className="py-6 w-full bg-red-200">
-        <div className="flex justify-between items-center max-w-screen-xl mx-auto">
-          <Link href="/">Quillhub</Link>
+      <header className="flex justify-between items-center max-w-screen-2xl mx-auto px-10 py-6 w-full sticky top-0 bg-white">
+        <Link
+          href="/"
+          className="flex items-center font-semibold text-lg text-teal-500 group"
+        >
+          QuillHub
+          <QuillHubLogo className="text-2xl group-hover:rotate-[10deg] duration-300" />
+        </Link>
 
-          {token ? (
-            <AuthSignOut authenticatedUser={token} />
-          ) : (
-            <Link href="/auth/signin">Sign In</Link>
-          )}
-        </div>
+        {token ? (
+          <AuthSignOut authenticatedUser={token} />
+        ) : (
+          <Link href="/auth/signin">Sign In</Link>
+        )}
       </header>
 
-      <main className="container mx-auto max-w-screen-xl">{children}</main>
+      <main className="container mx-auto max-w-screen-2xl ">{children}</main>
     </>
   );
 }
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,65 @@
-export default function Home() {
+import { BlogPostCard } from "@/components/BlogPost/BlogPostCard";
+import Link from "next/link";
+import { FaArrowRightLong as SeeAllIcon } from "react-icons/fa6";
+
+export default async function Home() {
   return (
-    <div>
-      <h1 className="text-5xl font-bold">QuillHub</h1>
-      <p>Unfold Your Imagination, Share Your Universe</p>
-    </div>
+    <main>
+      {/* CTA section */}
+      <section className="px-24 py-48 flex flex-col items-center justify-center text-center">
+        <h1 className="text-[64px] font-bold">QuillHub</h1>
+        <p className="text-slate-500 mb-12 text-sm">
+          <em>Unfold Your Imagination, Share Your Universe</em>
+        </p>
+
+        <p className="text-slate-500 mb-4">
+          Welcome to QuillHub, where your words come to life and your stories
+          find their wings. 🚀
+        </p>
+
+        <p className="text-slate-500 mb-12">
+          Ready to embark on a journey of self-expression and connection?
+          <br />
+          Join QuillHub today and start weaving your stories into the fabric of
+          our vibrant literary community.
+          <br />
+          <span className="text-slate-500 font-semibold">
+            <em>Your words matter, and here, they find their home</em>
+          </span>
+        </p>
+
+        <Link
+          href="/"
+          className="w-48 rounded-md p-3 bg-teal-500 text-white font-medium text-md hover:shadow-xl duration-200"
+        >
+          Start Writing!
+        </Link>
+      </section>
+
+      {/* TRENDING SECTION */}
+      <section className="px-24 py-24">
+        <h2 className="text-4xl font-semibold text-slate-600">Trending</h2>
+        <p className="text-slate-400 mb-12">
+          Checkout some of the blog posts that are trending lately!
+          <Link
+            href="/trending"
+            className="ml-2 text-teal-500 inline-flex items-center"
+          >
+            See All
+            <SeeAllIcon className="ml-2" />
+          </Link>
+        </p>
+
+        <div className="grid grid-cols-2 gap-14">
+          <BlogPostCard />
+          <BlogPostCard />
+          <BlogPostCard />
+          <BlogPostCard />
+        </div>
+      </section>
+
+      {/* TODO: BOOKMARKED SECTION */}
+      {/* TODO: FOR YOU SECTION */}
+    </main>
   );
 }
