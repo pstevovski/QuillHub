4f21d214284670bdfea4b1e598f084d5941376a7
Petar Stevovski <stevovski.petar@gmail.com>
2024-01-21 00:12:30 +0100

    Updated styling for layout header; added footer

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -28,8 +28,17 @@ export default async function MainLayout({
           <Link href="/auth/signin">Sign In</Link>
         )}
       </header>
+      <hr className="border-none sticky top-[76px] max-w-screen-2xl mx-auto w-full h-[1px] bg-gradient-to-r from-white via-slate-200 to-white mb-6" />
 
       <main className="container mx-auto max-w-screen-2xl ">{children}</main>
+
+      <footer className="max-w-screen-2xl w-full mx-auto p-4 flex flex-col justify-center items-center">
+        <hr className="border-none w-full h-[1px] bg-gradient-to-r from-white via-slate-200 to-white mb-6" />
+        <Link href="/" className="flex items-center text-teal-500 group">
+          <span className="mr-2">&copy;</span>
+          QuillHub, {new Date().getFullYear()}
+        </Link>
+      </footer>
     </>
   );
 }
