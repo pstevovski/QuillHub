c0cdddb0804a456d9808058353ff95ef1dceb03f
Petar Stevovski <petar@codeart.mk>
2024-02-06 16:33:06 +0100

    Updates to 'Post - Create' page: - Added Status and Topic dropdowns - Added handling of selection for these two fields - Updated validation schema

--- src/app/(main)/post/create/page.tsx
+++ src/app/(main)/post/create/page.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import Button from "@/components/Buttons/Button";
+import DropdownLabel from "@/components/Dropdown/DropdownLabel";
+import DropdownSelect from "@/components/Dropdown/Select/DropdownSelect";
+import { DropdownSelectClickedItem } from "@/components/Dropdown/Select/DropdownSelectItem";
+import FormFieldErrorMessage from "@/components/Form/FormFieldErrorMessage";
 import FormTextInput from "@/components/Form/FormTextInput";
 import { PostsNew } from "@/db/schema/posts";
 import {
@@ -9,6 +14,7 @@ import {
 } from "@/utils/convertFileSize";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 // Assets
@@ -16,21 +22,32 @@ import { FaArrowLeftLong as GoBackIcon } from "react-icons/fa6";
 import { z } from "zod";
 
 const PostCreateSchema = z.object({
-  title: z.string({
-    required_error: "Please enter the title for the blog post",
-  }),
+  title: z
+    .string({
+      required_error: "Please enter the title for the blog post",
+    })
+    .min(5, "Please enter a title that is at least 5 characters long"),
   content: z.string({
     required_error: "Please enter the content of the blog post",
   }),
-  status: z.enum(["Draft", "Published"]).default("Draft"),
+  status: z
+    .enum(["draft", "published"], {
+      required_error: "Please select the status of the blog post",
+    })
+    .default("draft"),
   cover_photo: z
     .custom<File>()
-    .refine((file) => !file, "Cover photo is required.")
     .refine((file) => {
+      if (!file) return false;
+      return true;
+    }, "Cover photo is required.")
+    .refine((file) => {
+      if (!file) return false;
       // Check the file size
       return MAX_IMAGE_SIZE > convertFileSize(file.size);
     }, `Maximum image file size is ${MAX_IMAGE_SIZE}MB.`)
     .refine((file) => {
+      if (!file) return false;
       // Check if the file type is supportewd
       return SUPPORTED_IMAGE_TYPES.includes(file.type);
     }, "Selected image file type is not supported"),
@@ -43,6 +60,7 @@ export default function PostCreate() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm<PostsNew>({
     defaultValues: {
@@ -50,9 +68,39 @@ export default function PostCreate() {
       cover_photo_url: null,
       content: "",
       status: "draft",
+      topic_id: undefined,
     },
     resolver: zodResolver(PostCreateSchema),
   });
+
+  /*================================
+    BLOG POST STATUS
+  =================================*/
+  const [status, setStatus] = useState<DropdownSelectClickedItem[]>([
+    { text: "Draft", value: "draft" },
+  ]);
+  const handleStatusSelection = (selectedStatus: DropdownSelectClickedItem) => {
+    // Updates the inner selection state of the dropdown component
+    setStatus([selectedStatus]);
+
+    // Updates the value to be sent in the form
+    // todo: figure a way to autocast the selected value to the defined schema
+    setValue("status", selectedStatus.value as "draft" | "published");
+  };
+
+  /*================================
+    BLOG POST TOPIC
+
+    Note: Currently using fake data.
+  =================================*/
+  const [topic, setTopic] = useState<DropdownSelectClickedItem[]>([]);
+  const handleTopicSelection = (selectedTopic: DropdownSelectClickedItem) => {
+    // Updates the inner selection state of the dropdown component
+    setTopic([selectedTopic]);
+
+    // Updates the value to be sent in the form
+    setValue("topic_id", parseInt(selectedTopic.value as string));
+  };
 
   const handlePostCreate: SubmitHandler<PostsNew> = async (values) => {
     console.log("creating blog post...", values);
@@ -85,6 +133,54 @@ export default function PostCreate() {
           autoComplete="title"
           modifierClass="max-w-lg"
         />
+
+        {/* STATUS */}
+        <DropdownSelect
+          selection={status}
+          handleSelection={handleStatusSelection}
+        >
+          <DropdownLabel>Status</DropdownLabel>
+          <DropdownSelect.Trigger
+            loading={false}
+            disabled={false}
+            placeholderText="Select Status"
+            modifierClass={errors.status ? "border-red-600" : ""}
+          />
+          <DropdownSelect.Body>
+            <DropdownSelect.Item value="draft">Draft</DropdownSelect.Item>
+            <DropdownSelect.Item value="published">
+              Published
+            </DropdownSelect.Item>
+          </DropdownSelect.Body>
+          <FormFieldErrorMessage error={errors.status} />
+        </DropdownSelect>
+
+        {/* TOPIC */}
+        <DropdownSelect
+          selection={topic}
+          handleSelection={handleTopicSelection}
+        >
+          <DropdownLabel>Topic</DropdownLabel>
+          <DropdownSelect.Trigger
+            loading={false}
+            disabled={false}
+            placeholderText="Select Topic"
+            modifierClass={errors.topic_id ? "border-red-600" : ""}
+          />
+          <DropdownSelect.Body>
+            <DropdownSelect.Item value="1">Topic #1</DropdownSelect.Item>
+            <DropdownSelect.Item value="2">Topic #2</DropdownSelect.Item>
+            <DropdownSelect.Item value="3">Topic #3</DropdownSelect.Item>
+            <DropdownSelect.Item value="4">Topic #4</DropdownSelect.Item>
+            <DropdownSelect.Item value="5">Topic #5</DropdownSelect.Item>
+          </DropdownSelect.Body>
+          <FormFieldErrorMessage error={errors.topic_id} />
+        </DropdownSelect>
+
+        {/* Todo: Add file upload for "Cover Photo" field */}
+        {/* Todo: Add Textarea for "Content" field, change with WYSIWIG later */}
+
+        <Button type="submit">Create Post</Button>
       </form>
     </div>
   );
