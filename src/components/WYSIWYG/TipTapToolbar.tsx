7cb74da9f79c32fc72d21948f37c26a3a771f16b
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-08 23:01:57 +0100

    Added usage of more extensions (plugins) and interactivity with them trough the UI; added TODO comments

--- src/components/WYSIWYG/TipTapToolbar.tsx
+++ src/components/WYSIWYG/TipTapToolbar.tsx
@@ -11,6 +11,21 @@ import {
   Heading5,
   Heading6,
   Bold,
+  List,
+  ListOrdered,
+  Quote,
+  Undo2,
+  Redo2,
+  Italic,
+  Strikethrough,
+  SeparatorHorizontal,
+  Underline,
+  AlignLeft,
+  AlignCenter,
+  AlignRight,
+  AlignJustify,
+  Code as CodeInline,
+  Code2 as CodeBlock,
 } from "lucide-react";
 
 interface TipTapToolbarProps {
@@ -86,6 +101,156 @@ export default function TipTapToolbar({ editor }: TipTapToolbarProps) {
       >
         <Bold />
       </Toggle>
+
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("bulletList")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleBulletList().run();
+        }}
+      >
+        <List />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("orderedList")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleOrderedList().run();
+        }}
+      >
+        <ListOrdered />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("blockquote")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleBlockquote().run();
+        }}
+      >
+        <Quote />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("undo")}
+        onPressedChange={() => {
+          editor.chain().focus().undo().run();
+        }}
+      >
+        <Undo2 />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("redo")}
+        onPressedChange={() => {
+          editor.chain().focus().redo().run();
+        }}
+      >
+        <Redo2 />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("italic")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleItalic().run();
+        }}
+      >
+        <Italic />
+      </Toggle>
+
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("strike")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleStrike().run();
+        }}
+      >
+        <Strikethrough />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("horizontalRule")}
+        onPressedChange={() => {
+          editor.chain().focus().setHorizontalRule().run();
+        }}
+      >
+        <SeparatorHorizontal />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("underline")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleUnderline().run();
+        }}
+      >
+        <Underline />
+      </Toggle>
+
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("code")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleCode().run();
+        }}
+      >
+        <CodeInline />
+      </Toggle>
+
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("codeblock")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleCodeBlock().run();
+        }}
+      >
+        <CodeBlock />
+      </Toggle>
+
+      {/* TODO: Convert this to dropdown menu */}
+      <Toggle
+        size="lg"
+        pressed={editor.isActive({ textAlign: "left" })}
+        onPressedChange={() => {
+          editor.chain().focus().setTextAlign("left").run();
+        }}
+      >
+        <AlignLeft />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive({ textAlign: "center" })}
+        onPressedChange={() => {
+          editor.chain().focus().setTextAlign("center").run();
+        }}
+      >
+        <AlignCenter />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive({ textAlign: "right" })}
+        onPressedChange={() => {
+          editor.chain().focus().setTextAlign("right").run();
+        }}
+      >
+        <AlignRight />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive({ textAlign: "justify" })}
+        onPressedChange={() => {
+          editor.chain().focus().setTextAlign("justify").run();
+        }}
+      >
+        <AlignJustify />
+      </Toggle>
+
+      {/* TODO: Add Link extension together with custom dialog box for handling the URL and target*/}
+      {/* TODO: Add Color extension together with custom trigger for color input selection */}
+      {/* TODO: 
+          Add Image extension together with custom functionality for handling image upload / drop from outside
+        - https://www.codemzy.com/blog/tiptap-drag-drop-image
+      */}
+      {/* TODO: Add Youtube extension (??) */}
+      {/* TODO: Add shortcuts info icon that will open a dialog (modal) listing all of the shortcuts available */}
     </div>
   );
 }
