6417b2f86c1baf50a699eb385b5a6b32f81ac8bf
Petar Stevovski <petar@codeart.mk>
2024-02-08 16:50:17 +0100

    Testing Toolbar creation for TipTap editor

--- src/components/WYSIWYG/TipTapToolbar.tsx
+++ src/components/WYSIWYG/TipTapToolbar.tsx
@@ -1,5 +1,17 @@
 "use client";
+import { Toggle } from "@/ui/toggle";
 import { type Editor } from "@tiptap/react";
+
+// Assets
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+  Bold,
+} from "lucide-react";
 
 interface TipTapToolbarProps {
   editor: Editor | null;
@@ -11,20 +23,69 @@ export default function TipTapToolbar({ editor }: TipTapToolbarProps) {
 
   return (
     <div className="border rounded-md border-input bg-transparent">
-      <span
-        onClick={() => {
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("heading", { level: 1 })}
+        onPressedChange={() => {
           editor.chain().focus().toggleHeading({ level: 1 }).run();
         }}
       >
-        H1
-      </span>
-      <span
-        onClick={() => {
+        <Heading1 />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("heading", { level: 2 })}
+        onPressedChange={() => {
+          editor.chain().focus().toggleHeading({ level: 2 }).run();
+        }}
+      >
+        <Heading2 />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("heading", { level: 3 })}
+        onPressedChange={() => {
+          editor.chain().focus().toggleHeading({ level: 3 }).run();
+        }}
+      >
+        <Heading3 />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("heading", { level: 4 })}
+        onPressedChange={() => {
+          editor.chain().focus().toggleHeading({ level: 4 }).run();
+        }}
+      >
+        <Heading4 />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("heading", { level: 5 })}
+        onPressedChange={() => {
+          editor.chain().focus().toggleHeading({ level: 5 }).run();
+        }}
+      >
+        <Heading5 />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("heading", { level: 6 })}
+        onPressedChange={() => {
+          editor.chain().focus().toggleHeading({ level: 6 }).run();
+        }}
+      >
+        <Heading6 />
+      </Toggle>
+      <Toggle
+        size="lg"
+        pressed={editor.isActive("bold")}
+        onPressedChange={() => {
           editor.chain().focus().toggleBold().run();
         }}
       >
-        Bold
-      </span>
+        <Bold />
+      </Toggle>
     </div>
   );
 }
