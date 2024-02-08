cc95aa52291e876e057acb2c93c853876d22b29e
Petar Stevovski <petar@codeart.mk>
2024-02-08 16:20:02 +0100

    Updated classnames utility function

--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,8 +1,6 @@
-import clsx from "clsx";
-import { ClassValue } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/** Utility function for merging tailwind classess preventing conflicts caused by order */
-export default function cn(...inputs: ClassValue[]): string {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
