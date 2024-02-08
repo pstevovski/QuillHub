283b784e2cbcd8edd2ab38a5df20ee9ebe167570
Petar Stevovski <petar@codeart.mk>
2024-02-08 11:07:26 +0100

    Updated FormUpload component to include functionality for previewing the image that was selected for upload, displaying the selected file details and selection clearing. Removed unused FormUploadPreview component

--- src/components/Form/FormUpload.tsx
+++ src/components/Form/FormUpload.tsx
@@ -1,27 +1,57 @@
-import { InputHTMLAttributes, useRef } from "react";
+import { InputHTMLAttributes, useEffect, useRef, useState } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
+import { convertFileSize } from "@/utils/convertFileSize";
 
 // Assets
 import { HiOutlineUpload as FileUploadIcon } from "react-icons/hi";
+import { IoTrashBinOutline as FileUploadClearIcon } from "react-icons/io5";
+
 import Button from "../Buttons/Button";
 
 interface FormUploadProps extends InputHTMLAttributes<HTMLInputElement> {
   register: UseFormRegisterReturn;
+  selectedFile: File | null;
+  handleClearSelection: () => void;
   placeholderText?: string;
   modifierClass?: string;
+  hasPreview?: boolean;
   maxSizeInMB?: number;
 }
 
-export default function FormUpload({
+function FormUpload({
   register,
+  selectedFile,
   placeholder = "",
   modifierClass = "",
   maxSizeInMB = 5,
+  hasPreview = false,
+  handleClearSelection,
   ...props
 }: FormUploadProps) {
   const fileUploadFieldRef = useRef<HTMLInputElement | null>(null);
 
-  // todo: add option for image upload preview
+  /*======================
+    PREVIEW DETAILS
+  =======================*/
+  const [fileDetails, setFileDetails] = useState<File | null>(null);
+  const [thumbnailPreview, setThumbnailPreview] = useState<string | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    setFileDetails(selectedFile);
+  }, [selectedFile]);
+
+  // Generate a thumbnail preview image
+  useEffect(() => {
+    if (!fileDetails || !hasPreview) {
+      setThumbnailPreview(undefined);
+      return;
+    }
+
+    const fileObjectURL = URL.createObjectURL(fileDetails);
+    setThumbnailPreview(fileObjectURL);
+  }, [fileDetails]);
 
   return (
     <div className="relative">
@@ -38,16 +68,47 @@ export default function FormUpload({
         {...props}
       />
 
-      <Button
-        type="button"
-        size="sm"
-        onClick={() => {
-          fileUploadFieldRef.current?.click();
-        }}
-      >
-        Upload
-        <FileUploadIcon className="text-xl ml-2" />
-      </Button>
+      {/* FILE UPLOAD PREVIEW */}
+      {thumbnailPreview ? (
+        <img
+          className="block max-w-[350px] max-h-[350px] w-full h-full rounded-md"
+          src={thumbnailPreview}
+        />
+      ) : null}
+
+      {/* FILE DETAILS */}
+      {fileDetails ? (
+        <span className="text-sm text-slate-500">
+          {fileDetails.name}, {convertFileSize(fileDetails.size)}MB
+        </span>
+      ) : null}
+
+      <div className="flex items-center">
+        <Button
+          type="button"
+          size="sm"
+          onClick={() => {
+            fileUploadFieldRef.current?.click();
+          }}
+        >
+          Upload
+          <FileUploadIcon className="text-xl ml-2" />
+        </Button>
+
+        {fileDetails ? (
+          <div
+            className="flex items-center group cursor-pointer"
+            onClick={handleClearSelection}
+          >
+            <FileUploadClearIcon className="text-slate-400 text-lg ml-2 group-hover:text-red-500 duration-300" />
+            <span className="text-slate-400 text-sm ml-1 group-hover:text-red-500 duration-300">
+              Clear Selection?
+            </span>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
+
+export default FormUpload;
