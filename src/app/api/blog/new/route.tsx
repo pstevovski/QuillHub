587d62a184365f2743e9726f9f6b99937f576226
Petar Stevovski <petar@codeart.mk>
2024-02-12 16:14:21 +0100

    Added API endpoint for creating new blog posts

---
+++ src/app/api/blog/new/route.tsx
@@ -0,0 +1,34 @@
+import BlogPostsService from "@/services/posts";
+import handleErrorMessage from "@/utils/handleErrorMessage";
+import { BlogNewPostSchema } from "@/zod/blog-posts";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const blogPostPayload = await request.json();
+    const validatePayload = BlogNewPostSchema.safeParse({
+      title: blogPostPayload.title,
+      content: blogPostPayload.content,
+      status: blogPostPayload.status,
+    });
+
+    if (!validatePayload) {
+      return Response.json(
+        { error: "Invalid values provided!" },
+        { status: 422 }
+      );
+    }
+
+    await BlogPostsService.create(blogPostPayload);
+
+    return NextResponse.json(
+      { message: "Blog post created!" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: handleErrorMessage(error) },
+      { status: 500 }
+    );
+  }
+}
