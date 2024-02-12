35cd4e719b641035912965849fcd448266b27979
Petar Stevovski <petar@codeart.mk>
2024-02-12 16:53:12 +0100

    Added method for deleting a blog post, alongside with all of the attached photos that it has. WIP - needs api endpoint

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,6 +1,9 @@
+import "dotenv/config";
+
 import db from "@/db/connection";
 import { postsImagesSchema, postsSchema } from "@/db/schema/posts";
 import handleErrorMessage from "@/utils/handleErrorMessage";
+import { eq } from "drizzle-orm";
 import { UploadFileResponse } from "uploadthing/client";
 
 interface BlogNewPostPayload {
@@ -46,6 +49,48 @@ class BlogPosts {
         `Failed creating new blog post: ${handleErrorMessage(error)}`
       );
       throw new Error("Failed creating new blog post!");
+    }
+  }
+
+  /**
+   * Delete an existing blog post along with
+   * all images that were attached to it and uploaded
+   * via UploadThing
+   *
+   * @param id - The corresponding Blog Post ID
+   * @type integer
+   *
+   */
+  async delete(blogPostID: number) {
+    try {
+      // Get all attached images keys that belong to the specific blog post
+      const attachedImages = await db
+        .select()
+        .from(postsImagesSchema)
+        .where(eq(postsImagesSchema.post_id, blogPostID));
+
+      // If there are any attached images, remove them from UploadThing's servers
+      if (attachedImages.length > 0) {
+        // Extract the file keys
+        const fileKeys = attachedImages.map((image) => image.key);
+
+        fetch("https://uploadthing.com/api/deleteFile", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-Uploadthing-Api-Key": process.env.UPLOADTHING_SECRET || "",
+          },
+          body: JSON.stringify({ fileKeys }),
+        });
+      }
+
+      // Remove blog post from the database
+      await db.delete(postsSchema).where(eq(postsSchema.id, blogPostID));
+
+      // todo: add API endpoint for this
+    } catch (error) {
+      console.log(`Failed deleting blog post: ${handleErrorMessage(error)}`);
+      throw new Error("Failed deleting blog post!");
     }
   }
 }
