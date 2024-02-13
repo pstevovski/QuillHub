b97b36319907de142178095b0d4ffbae7ef3bfc5
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-13 16:40:48 +0100

    Updates to Blog Posts service: - Added 'getImageKeys' private method that will extract the keys of those images that were uploaded and are either designated to saved in database (used in blog post) or deleted from uploadthing servers (not used in blog post) - Added 'deleteImagesFromUploadthing' method that triggers an API request to remove the uploaded images from Uploadthing servers - Updated 'create' method to be able to handle removing those images that were uploaded to Uploadthing servers while creating the blog post but ended up not being part of the final version of the blog post

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -4,17 +4,76 @@ import db from "@/db/connection";
 import { postsImagesSchema, postsSchema } from "@/db/schema/posts";
 import handleErrorMessage from "@/utils/handleErrorMessage";
 import { eq } from "drizzle-orm";
-import { UploadFileResponse } from "uploadthing/client";
 
 interface BlogNewPostPayload {
   title: string;
   status: "draft" | "published";
   content: string;
   cover_photo: string;
-  content_images: UploadFileResponse<unknown>[];
+  uploaded_content_images_keys: string[];
+  uploaded_cover_images_keys: string[];
 }
 
 class BlogPosts {
+  /**
+   *
+   * Get the keys of the uploaded images.
+   *
+   * `imagesToBeRemoved` contains the keys of those images
+   * that were uploaded to Uploadthing servers but are not used in the blog post
+   *
+   * `imagesToBeSaved` contains the keys of those images
+   * that were uploaded to Uploadthing servers and will be used in the blog post
+   *
+   * @param keys List of keys, either `cover` or `content` images keys
+   * @param source Check against this source, can be either the `content` or the `cover_photo`
+   */
+  private getImageKeys(keys: string[], source: string) {
+    if (!keys.length || !source)
+      return {
+        imagesToBeSaved: [],
+        imagesToBeRemoved: [],
+      };
+
+    const imagesToBeRemoved: string[] = [...keys].filter((key) => {
+      return !source.includes(key);
+    });
+
+    const imagesToBeSaved: string[] = [...keys].filter((key) => {
+      return source.includes(key);
+    });
+
+    return { imagesToBeRemoved, imagesToBeSaved };
+  }
+
+  /**
+   *
+   * Deletes images that were uploaded to Uploadthing servers
+   * but won't be used anymore, in order to free up unused space.
+   *
+   * @param imageKeys List of unique `file keys` corresponding
+   * to the files that will be deleted from Uploadthing servers.
+   *
+   */
+  private async deleteImagesFromUploadthing(imageKeys: string[]) {
+    try {
+      await fetch("https://uploadthing.com/api/deleteFile", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Uploadthing-Api-Key": process.env.UPLOADTHING_SECRET || "",
+        },
+        body: JSON.stringify({ fileKeys: imageKeys }),
+      });
+
+      console.log(
+        "Unused images successfully removed from Uploadthing servers."
+      );
+    } catch (error) {
+      throw new Error("Failed deleting unused images!");
+    }
+  }
+
   /**
    * Create a new blog post
    */
@@ -22,7 +81,8 @@ class BlogPosts {
     title,
     content,
     status,
-    content_images,
+    uploaded_cover_images_keys,
+    uploaded_content_images_keys,
     cover_photo,
   }: BlogNewPostPayload) {
     try {
@@ -35,15 +95,45 @@ class BlogPosts {
         views: 0,
       });
 
-      // Append the ID of the created post to each of the attached images
-      const attachedImages = content_images.map((attachedImage) => {
+      // Extract the keys for those cover and content images
+      // that are to be saved in the database and
+      // the ones that are to be remvoed from Uploadthing's servers
+      const uploadedCoverImages = this.getImageKeys(
+        uploaded_cover_images_keys,
+        cover_photo
+      );
+      const uploadedContentImages = this.getImageKeys(
+        uploaded_content_images_keys,
+        content
+      );
+
+      // Map the keys of those images that will be saved in the database
+      // to include the ID of the newly created post
+      const uploadedImagesKeys = [
+        ...uploadedCoverImages.imagesToBeSaved,
+        ...uploadedContentImages.imagesToBeSaved,
+      ].map((imageKey) => {
         return {
-          ...attachedImage,
+          key: imageKey,
           post_id: newPost[0].insertId,
         };
       });
-      await db.insert(postsImagesSchema).values(attachedImages);
-      console.log("Created new blog post with ID: ", newPost[0].insertId);
+      await db.insert(postsImagesSchema).values(uploadedImagesKeys);
+
+      console.log("Blog post successfully created!");
+      console.log("");
+      console.log("");
+
+      // Delete images that were uploaded while user was creating the blog post
+      // but ended up not being used in the final version of the post
+      console.log(
+        "Deleting unused images that are uploaded to Uploadthing servers..."
+      );
+      this.deleteImagesFromUploadthing(uploadedCoverImages.imagesToBeRemoved); // Cover images
+      this.deleteImagesFromUploadthing(uploadedContentImages.imagesToBeRemoved); // Content images
+      console.log(
+        "Unused images successfully deleted from Uploadthing servers!"
+      );
     } catch (error) {
       console.log(
         `Failed creating new blog post: ${handleErrorMessage(error)}`
