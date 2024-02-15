655f2a322fad4f5c598189ad0fcf74c10b469cf0
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-15 14:29:29 +0100

    Removed old migrations and generated new ones after updates to schema

--- src/db/migrations/0002_moaning_professor_monster.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `roles` MODIFY COLUMN `id` bigint AUTO_INCREMENT NOT NULL;--> statement-breakpoint
-ALTER TABLE `user_password_resets` MODIFY COLUMN `id` bigint AUTO_INCREMENT NOT NULL;--> statement-breakpoint
-ALTER TABLE `users` MODIFY COLUMN `id` bigint AUTO_INCREMENT NOT NULL;
