228fcf5e8ca66a20466f4e0e4fd45e2c15581513
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-15 14:53:07 +0100

    Decode the access token and extract the currently logged in user's ID, to be included in the payload for creating a new blog post

--- src/app/api/blog/route.tsx
+++ src/app/api/blog/route.tsx
@@ -1,4 +1,5 @@
 import BlogPostsService from "@/services/posts";
+import TokenService from "@/services/token";
 import handleErrorMessage from "@/utils/handleErrorMessage";
 import { BlogNewPostSchema } from "@/zod/blog-posts";
 import { NextResponse } from "next/server";
@@ -17,14 +18,20 @@ export async function POST(request: Request) {
       status: blogPostPayload.status,
     });
 
-    if (!validatePayload) {
+    // Extract the ID of the currently logged in user
+    const decodedToken = await TokenService.decodeToken();
+
+    if (!validatePayload || !decodedToken) {
       return Response.json(
         { error: "Invalid values provided!" },
         { status: 422 }
       );
     }
 
-    await BlogPostsService.create(blogPostPayload);
+    await BlogPostsService.create({
+      ...blogPostPayload,
+      user_id: decodedToken.id,
+    });
 
     return NextResponse.json(
       { message: "Blog post created!" },
