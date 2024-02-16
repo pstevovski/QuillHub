09cc3404a306cde37cc1c1a51e50fcd0e2a63f01
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-16 11:20:16 +0100

    Removed unnecessary bridge table for topics and posts relationship; updated 'topics' schema so it will default the 'created_by' column to NULL if the user that created the topic was removed

--- src/db/schema/topics.ts
+++ src/db/schema/topics.ts
@@ -1,26 +1,13 @@
 import { bigint, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
 import { users } from "./users";
-import { postsSchema } from "./posts";
 
 export const schemaTopics = mysqlTable("topics", {
   id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
   name: varchar("name", { length: 32 }).unique().notNull(),
   label: varchar("label", { length: 32 }).notNull(),
-  created_by: bigint("created_by", { mode: "number" })
-    .notNull()
-    .references(() => users.id),
+  created_by: bigint("created_by", { mode: "number" }).references(
+    () => users.id,
+    { onDelete: "set null" }
+  ),
   created_at: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
-});
-
-/*====================================
-  TABLE: BLOG POST TOPICS 
-=====================================*/
-export const schemaTopicsBlogPosts = mysqlTable("topics_blog_posts", {
-  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-  post_id: bigint("post_id", { mode: "number" }).references(
-    () => postsSchema.id
-  ),
-  topic_id: bigint("topic_id", { mode: "number" }).references(
-    () => schemaTopics.id
-  ),
 });
