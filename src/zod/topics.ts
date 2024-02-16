dcb726bfd5b240f63bebab9c787e997fa1aa3a90
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-16 14:21:06 +0100

    Added regex validation to the topics validation schema to prevent users from trying to create a topic whose name contains special characters

--- src/zod/topics.ts
+++ src/zod/topics.ts
@@ -4,5 +4,6 @@ export const VALIDATION_SCHEMA_TOPICS = z.object({
   name: z
     .string({ required_error: "Topic name must be provided!" })
     .min(3, "Topic name too short!")
-    .max(32, "Topic name too long!"),
+    .max(32, "Topic name too long!")
+    .regex(/^[A-Za-z0-9 ]*$/gi, { message: "No special characters allowed!" }),
 });
