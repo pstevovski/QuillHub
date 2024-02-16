b45679b70faec5e7781a4e4f4e8fba6917477e77
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-16 15:13:11 +0100

    Added method for handling topic name updates which ensures that the update can only be triggered by an admin user or the user that created the topic

--- src/services/topics.ts
+++ src/services/topics.ts
@@ -2,6 +2,12 @@ import handleErrorMessage from "@/utils/handleErrorMessage";
 import db from "@/db/connection";
 import { schemaTopics } from "@/db/schema/topics";
 import { eq, inArray } from "drizzle-orm";
+import TokenService from "./token";
+
+enum UserRoles {
+  BASIC = 1,
+  ADMIN = 2,
+}
 
 class Topics {
   private generateUniqueTopicSlug(name: string): string {
@@ -34,7 +40,7 @@ class Topics {
     }
   }
 
-  async createTopic(userId: number, name: string) {
+  async create(userId: number, name: string) {
     try {
       await db.insert(schemaTopics).values({
         name,
@@ -46,6 +52,42 @@ class Topics {
     } catch (error) {
       console.log(`Failed creating topic: ${handleErrorMessage(error)}`);
       throw new Error("Failed creating topic");
+    }
+  }
+
+  async update(id: number, name: string) {
+    try {
+      const tokenDetails = await TokenService.decodeToken();
+      const userID = tokenDetails?.id || 0;
+      const roleID = tokenDetails?.role_id || 1;
+
+      // Find the targeted topic
+      const targetedTopic = await db
+        .select()
+        .from(schemaTopics)
+        .where(eq(schemaTopics.id, id));
+
+      if (!targetedTopic[0]) return null;
+
+      // Only allow the user that created the topic (or an admin) to update it
+      if (
+        targetedTopic[0].created_by !== userID ||
+        roleID !== UserRoles.ADMIN
+      ) {
+        throw new Error("Unauthorized to update this topic!");
+      }
+
+      // Update the name of the topic
+      await db
+        .update(schemaTopics)
+        .set({ name })
+        .where(eq(schemaTopics.id, id));
+      console.log(`Successfully updated the name of topic with ID ${id}.`);
+    } catch (error) {
+      console.log(
+        `Failed updating topic with ID ${id}: ${handleErrorMessage(error)}`
+      );
+      throw new Error(handleErrorMessage(error));
     }
   }
 
