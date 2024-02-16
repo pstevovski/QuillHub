cda60a0ec0d6335135065c0baf9159b9f9051940
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-16 14:20:38 +0100

    Updated TopicsService to include method for getting all currently existing topics; updated handling of user ID used when creating a new topic

--- src/services/topics.ts
+++ src/services/topics.ts
@@ -1,5 +1,4 @@
 import handleErrorMessage from "@/utils/handleErrorMessage";
-import TokenService from "./token";
 import db from "@/db/connection";
 import { schemaTopics } from "@/db/schema/topics";
 import { eq } from "drizzle-orm";
@@ -9,23 +8,13 @@ class Topics {
     return name.split(" ").join("-").toLowerCase();
   }
 
-  async createTopic(name: string) {
+  async getAll() {
     try {
-      // Get the ID of the logged in user
-      // Todo: This will become repetitive; extract functionality?
-      const userToken = await TokenService.decodeToken();
-      if (!userToken) throw new Error("Unauthenticated!");
-
-      await db.insert(schemaTopics).values({
-        name,
-        slug: this.generateUniqueTopicSlug(name),
-        created_by: userToken.id as number,
-      });
-
-      console.log("Topic created successfully!");
+      const topics = await db.select().from(schemaTopics);
+      return topics;
     } catch (error) {
-      console.log(`Failed creating topic: ${handleErrorMessage(error)}`);
-      throw new Error("Failed creating topic");
+      console.log(`Failed getting topics: ${handleErrorMessage(error)}`);
+      throw new Error("Failed getting topics");
     }
   }
 
@@ -36,16 +25,27 @@ class Topics {
         .from(schemaTopics)
         .where(eq(schemaTopics.id, id));
 
-      if (!targetedTopic || !targetedTopic[0]) {
-        throw new Error("Topic does not exist!");
-      }
-
       return targetedTopic[0];
     } catch (error) {
       console.log(
         `Failed getting topic with id ${id}: ${handleErrorMessage(error)}`
       );
       throw new Error("Failed getting the targeted topic");
+    }
+  }
+
+  async createTopic(userId: number, name: string) {
+    try {
+      await db.insert(schemaTopics).values({
+        name,
+        slug: this.generateUniqueTopicSlug(name),
+        created_by: userId,
+      });
+
+      console.log("Topic created successfully!");
+    } catch (error) {
+      console.log(`Failed creating topic: ${handleErrorMessage(error)}`);
+      throw new Error("Failed creating topic");
     }
   }
 }
