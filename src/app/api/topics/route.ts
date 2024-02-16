d6e261effd99635fa2e3cc275b200d35fcbb4547
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-16 14:19:52 +0100

    Added GET endpoint for getting the list of all existing topics; updated POST endpoint (for creating new topics) to extract the user ID of the current access token and pass it down to the service

--- src/app/api/topics/route.ts
+++ src/app/api/topics/route.ts
@@ -1,23 +1,61 @@
+import TokenService from "@/services/token";
 import TopicsService from "@/services/topics";
 import handleErrorMessage from "@/utils/handleErrorMessage";
 import { VALIDATION_SCHEMA_TOPICS } from "@/zod/topics";
 import { NextResponse } from "next/server";
 
+/**
+ *
+ * Get the list of all existing topics
+ *
+ */
+export async function GET() {
+  try {
+    const topics = await TopicsService.getAll();
+
+    if (!topics || !topics.length) {
+      return NextResponse.json({ error: "No topics found!" }, { status: 404 });
+    }
+
+    return NextResponse.json(topics, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: handleErrorMessage(error) },
+      { status: 500 }
+    );
+  }
+}
+
+/**
+ *
+ * Create a new topic
+ *
+ */
 export async function POST(request: Request) {
   try {
     const payload = await request.json();
-    const validateTopicPayload = VALIDATION_SCHEMA_TOPICS.safeParse({
+
+    // Check if the received payload passes validation schema
+    const isPayloadValid = VALIDATION_SCHEMA_TOPICS.safeParse({
       name: payload.name,
     });
 
-    if (!validateTopicPayload.success) {
+    if (!isPayloadValid.success) {
       return NextResponse.json(
         { error: "Invalid values provided!" },
         { status: 422 }
       );
     }
 
-    await TopicsService.createTopic(payload.name);
+    // Get the ID of the currently logged in user
+    // Note: This will become repetitive, find solution
+    const tokenDetails = await TokenService.decodeToken();
+
+    if (!tokenDetails) {
+      return NextResponse.json({ error: "Unauthenticated" }, { status: 401 });
+    }
+
+    await TopicsService.createTopic(tokenDetails.id as number, payload.name);
 
     return NextResponse.json({ message: "Topic created!" }, { status: 200 });
   } catch (error) {
