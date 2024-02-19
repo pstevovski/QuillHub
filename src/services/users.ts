b12b41ede540254eb8e350c1f55b40fbbfa9c83e
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-19 14:10:43 +0100

    Updated error handling for Users service 'getCurrentUser' method

--- src/services/users.ts
+++ src/services/users.ts
@@ -3,16 +3,17 @@ import TokenService from "./token";
 import db from "@/db/connection";
 import { eq } from "drizzle-orm";
 import handleErrorMessage from "@/utils/handleErrorMessage";
+import { ApiErrorMessage } from "@/app/api/handleApiError";
 
 class Users {
-  /** Gets the details for the currently logged in user */
-  async getCurrentUser(): Promise<User | null> {
+  /** Gets the details of the currently logged in user */
+  async getCurrentUser(): Promise<User> {
     try {
       // Decode the token saved as a cookie upon signin
       const userToken = await TokenService.decodeToken();
       const userID = userToken?.id as number;
 
-      if (!userID) return null;
+      if (!userID) throw new Error(ApiErrorMessage.UNAUTHENTICATED);
 
       // Get the user from the database
       const user: User[] = await db
@@ -20,13 +21,15 @@ class Users {
         .from(users)
         .where(eq(users.id, userID));
 
-      if (!user || !user[0]) return null;
+      if (!user || !user[0]) throw new Error(ApiErrorMessage.NOT_FOUND);
 
       return user[0];
     } catch (error) {
-      throw new Error(
-        `Failed getting currently logged-in user: ${handleErrorMessage(error)}`
+      console.log(
+        "USERS - Failed getting currently logged-in user: ",
+        handleErrorMessage(error)
       );
+      throw new Error(handleErrorMessage(error));
     }
   }
 }
