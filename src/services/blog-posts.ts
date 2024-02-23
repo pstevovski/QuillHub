ade397e34fa6c3e6c9cddbe356888694630ce41e
Petar Stevovski <stevovski.petar@gmail.com>
2024-02-23 17:03:57 +0100

    Updates to BlogPosts Service: - Added 'edit' method for handling updates of the targeted blog post - Updated 'create' method to remove unnecessary code

--- src/services/blog-posts.ts
+++ src/services/blog-posts.ts
@@ -37,20 +37,27 @@ class BlogPosts {
    * before saving the "post" / "image key" pairings inn the database
    *
    */
-  private async saveBlogPostImageKeys(postImageKeys: BlogPostSaveImageKeys) {
+  private async saveBlogPostImageKeys({
+    post_id,
+    cover_image_keys,
+    content_image_keys,
+  }: BlogPostSaveImageKeys) {
+    // If there are no image keys to be saved, exit early
+    if (content_image_keys.length === 0 && cover_image_keys.length === 0) {
+      return;
+    }
+
     try {
       // Filter out duplicates and merge the keys into a single array
-      const uniqueCoverImageKeys = [...new Set(postImageKeys.cover_image_keys)];
-      const uniqueContentImageKeys = [
-        ...new Set(postImageKeys.content_image_keys),
-      ];
+      const uniqueCoverImageKeys = [...new Set(cover_image_keys)];
+      const uniqueContentImageKeys = [...new Set(content_image_keys)];
       const uniqueKeys = [...uniqueCoverImageKeys, ...uniqueContentImageKeys];
 
       // Include the ID of the post to which the uploaded image key belongs
       const postKeyPairs = uniqueKeys.map((imageKey) => {
         return {
           key: imageKey,
-          post_id: postImageKeys.post_id,
+          post_id: post_id,
         };
       });
 
@@ -87,47 +94,85 @@ class BlogPosts {
         cover_photo,
         created_by: currentUser.id,
       });
-
-      // Extract the keys for those cover and content images
-      // that are to be saved in the database and
-      // the ones that are to be remvoed from Uploadthing's servers
-      const uploadedCoverImages = UploadService.handleImageKeys(
-        uploaded_cover_images_keys,
-        cover_photo
-      );
-      const uploadedContentImages = UploadService.handleImageKeys(
-        uploaded_content_images_keys,
-        content
-      );
       console.log("Blog post successfully created!");
 
       // Save the pairings between the image keys and the newly created post
       await this.saveBlogPostImageKeys({
         post_id: newPost[0].insertId,
-        content_image_keys: uploadedContentImages.imagesToBeSaved,
-        cover_image_keys: uploadedCoverImages.imagesToBeSaved,
+        cover_image_keys: UploadService.handleImageKeys(
+          uploaded_cover_images_keys,
+          cover_photo
+        ),
+        content_image_keys: UploadService.handleImageKeys(
+          uploaded_content_images_keys,
+          content
+        ),
       });
-
-      // Delete images that were uploaded while user was creating the blog post
-      // but ended up not being used in the final version of the post
-      console.log(
-        "Deleting unused images that are uploaded to Uploadthing servers..."
-      );
-
-      UploadService.deleteImagesFromUploadthing(
-        uploadedCoverImages.imagesToBeRemoved
-      );
-
-      UploadService.deleteImagesFromUploadthing(
-        uploadedContentImages.imagesToBeRemoved
-      );
-
-      console.log(
-        "Unused images successfully deleted from Uploadthing servers!"
-      );
     } catch (error) {
       console.log(
-        `Failed creating new blog post: ${handleErrorMessage(error)}`
+        `BLOG POSTS - Failed creating new blog post: ${handleErrorMessage(
+          error
+        )}`
+      );
+      throw new Error(handleErrorMessage(error));
+    }
+  }
+
+  /**
+   *
+   * Edit the targeted blog post
+   *
+   * @param blogPostID ID of the blog post to be updated
+   * @param {Object} updatedDetails
+   *
+   */
+  async edit(blogPostID: number, updatedDetails: BlogNewPostPayload) {
+    try {
+      // Check if the targeted blog post exists
+      const targetedBlogPost = await db
+        .select()
+        .from(postsSchema)
+        .where(eq(postsSchema.id, blogPostID));
+
+      if (!targetedBlogPost[0]) throw new Error(ApiErrorMessage.NOT_FOUND);
+
+      // Prevent updates to the blog post by users that did not create it
+      const currentUser = await UsersService.getCurrentUser();
+
+      if (targetedBlogPost[0].created_by !== currentUser.id) {
+        throw new Error(ApiErrorMessage.UNAUTHORIZED);
+      }
+
+      // Update the targeted blog post with the new details
+      await db
+        .update(postsSchema)
+        .set({
+          title: updatedDetails.title,
+          status: updatedDetails.status,
+          content: updatedDetails.content,
+          cover_photo: updatedDetails.cover_photo,
+          created_by: currentUser.id,
+        })
+        .where(eq(postsSchema.id, blogPostID));
+      console.log("Blog post successfully updated!");
+
+      // Update the pairings between the image keys and the updated post
+      await this.saveBlogPostImageKeys({
+        post_id: blogPostID,
+        cover_image_keys: UploadService.handleImageKeys(
+          updatedDetails.uploaded_cover_images_keys,
+          updatedDetails.cover_photo
+        ),
+        content_image_keys: UploadService.handleImageKeys(
+          updatedDetails.uploaded_content_images_keys,
+          updatedDetails.content
+        ),
+      });
+    } catch (error) {
+      console.log(
+        `BLOG POSTS - Failed editing blog post with ID ${blogPostID}: ${handleErrorMessage(
+          error
+        )}`
       );
       throw new Error(handleErrorMessage(error));
     }
